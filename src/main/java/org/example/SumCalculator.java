8909fe5bee941870b71ad6abe63f3404e8161942
VitaliiShtronda <v.shtronda@ukr.net>
2024-04-02 20:14:06 +0300

    Initial commit

---
+++ src/main/java/org/example/SumCalculator.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.lang.IllegalArgumentException;
+
+
+public class SumCalculator {
+
+
+        int sum(int n) {
+            if (n <= 0) throw new IllegalArgumentException();
+            int sum = 0;
+            for (int i = 0; i <= 0; i++) {
+                sum += i;
+            }
+            return sum;
+        }
+    }
