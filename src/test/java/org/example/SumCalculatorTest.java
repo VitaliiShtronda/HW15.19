625a39fba02fb029bed9e292fa4da15a70f6799c
VitaliiShtronda <v.shtronda@ukr.net>
2024-04-04 21:14:53 +0300

    New build

--- src/test/java/org/example/SumCalculatorTest.java
+++ src/test/java/org/example/SumCalculatorTest.java
@@ -1,4 +1,35 @@
 package org.example;
 
-public class SumCalculatorTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SumCalculatorTest {
+
+    public static SumCalculator calc;
+
+    @BeforeEach
+    void initCalc() {
+        calc = new SumCalculator();
+    }
+
+    @Test
+    void testSumOfOne() {
+        assertEquals(1, calc.sum(1));
+    }
+
+    @Test
+    void testSUmOfThree(){
+        assertEquals(6, calc.sum(3));
+    }
+
+    @Test
+    void testSumOfZero(){
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    calc.sum(0);
+                });
+    }
+
 }
